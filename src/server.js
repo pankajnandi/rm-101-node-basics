5b50632202594bdbf529abf58e6480ceb6d8ba38
pankajnandi <pkandpal28@gmail.com>
2022-06-20 19:21:52 +0530

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,43 @@
 // install and import express
-const express = () => {};
+const express = require("express");
+// const express = () => {};
 let app = express();
-
+app.use(express.json());
 // Code here
 
 // Note: Do not remove this export statement
+const data = require("./assets/user.json")
+app.get('/', function(req, res) {
+    res.sendFile('./assets/users.html', {root: __dirname })
+});
+app.get("/users", async (req, res) => {
+    try {
+      return res.status(200).send(data);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  });
+  app.get("/users/:id", async (req, res) => {
+    try {
+        const user = data.filter((e)=>e.id==req.params.id)
+      return res.status(200).send(user);
+    } catch (err) {
+      return res.status(500).send({ message: err.message });
+    }
+  });
+app.post("/users",async(req,res)=>{
+    try{
+        const user = data.push(req.body);
+        return res.status(200).send(user); 
+    }
+    catch(err){
+        return res.status(500).send({ message: err.message });
+
+    }
+})
+
+
+app.listen(8000, async function () {
+    console.log("Pankaj listening on port 8000");
+  });
 module.exports = app;
